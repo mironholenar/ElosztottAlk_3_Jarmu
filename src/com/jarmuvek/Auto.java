3069c7e487ecdb2d6c4489e389db167a7bc0f6b8
Shot846 <blaster2202@gmail.com>
2022-03-08 15:23:29 +0100

    +Auto class +Motor class

---
+++ src/com/jarmuvek/Auto.java
@@ -0,0 +1,10 @@
+package com.jarmuvek;
+
+public class Auto extends Jarmu{
+
+    public Auto(String nev, Szin szin, Ember tulajdonos, Uzemanyag uzemanyag) {
+        super(nev, szin, tulajdonos, uzemanyag);
+    }
+
+
+}
---
+++ src/com/jarmuvek/Motor.java
@@ -0,0 +1,10 @@
+package com.jarmuvek;
+
+public class Motor extends Jarmu{
+
+    public Motor(String nev, Szin szin, Ember tulajdonos, Uzemanyag uzemanyag) {
+        super(nev, szin, tulajdonos, uzemanyag);
+    }
+
+
+}
