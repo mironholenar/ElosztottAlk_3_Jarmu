96b4ca4f7a346b3d97bdbe4c04fc27c6bd1d6831
Dániel Fervágner <daniel.fervagner@gmail.com>
2022-03-08 14:58:28 +0100

    Ember osztály kiegészítve névvel, korral

--- src/com/jarmuvek/Ember.java
+++ src/com/jarmuvek/Ember.java
@@ -2,4 +2,24 @@ package com.jarmuvek;
 
 public class Ember {
 
+    private String nev;
+    private int kor;
+
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+
 }
