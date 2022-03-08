3a8e1c85dee2ef5c9b5f809424f9fb520f13faf8
Shot846 <blaster2202@gmail.com>
2022-03-08 14:29:52 +0100

    +Hello World teszt

--- src/com/jarmuvek/Main.java
+++ src/com/jarmuvek/Main.java
@@ -3,6 +3,6 @@ package com.jarmuvek;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World test");
+	System.out.println("Hello World test2");
     }
 }
