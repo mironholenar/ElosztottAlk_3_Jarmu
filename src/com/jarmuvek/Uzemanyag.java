142f65aabb7fde67f3dda992ca907cc7b0f8347c
Shot846 <blaster2202@gmail.com>
2022-03-28 18:39:46 +0200

    +Uzemanyag kiegeszitve

--- src/com/jarmuvek/Uzemanyag.java
+++ src/com/jarmuvek/Uzemanyag.java
@@ -2,15 +2,16 @@ package com.jarmuvek;
 
 public class Uzemanyag {
 
-    public Uzemanyag(int mennyiseg) {
-        this.mennyiseg = mennyiseg;
-    }
-
     enum Tipus {
         Benzin, Dizel
     }
 
     private int mennyiseg;
+
+    public Uzemanyag(Tipus tipus, int mennyiseg) {
+
+        this.mennyiseg = mennyiseg;
+    }
 
     public void setMennyiseg(int mennyiseg) {
         this.mennyiseg = mennyiseg;
