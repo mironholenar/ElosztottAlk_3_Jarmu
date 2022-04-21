7faee237e44bcf3894d1bce2f81f5d9080067710
Shot846 <blaster2202@gmail.com>
2022-04-21 14:10:04 +0200

    +Auto kiegeszitve +Motor kiegeszitve

--- src/com/jarmuvek/Auto.java
+++ src/com/jarmuvek/Auto.java
@@ -5,4 +5,8 @@ public class Auto extends Jarmu{
     public Auto(String nev, Szin szin, Uzemanyag uzemanyag) {
         super(nev, szin, uzemanyag);
     }
+
+    public Auto(String nev, Szin szin, Uzemanyag uzemanyag, Ember tulajdonos, Ember sofor) {
+        super(nev, szin, uzemanyag, tulajdonos, sofor);
+    }
 }
--- src/com/jarmuvek/Motor.java
+++ src/com/jarmuvek/Motor.java
@@ -5,4 +5,8 @@ public class Motor extends Jarmu{
     public Motor(String nev, Szin szin, Uzemanyag uzemanyag) {
         super(nev, szin, uzemanyag);
     }
+
+    public Motor(String nev, Szin szin, Uzemanyag uzemanyag, Ember tulajdonos, Ember sofor) {
+        super(nev, szin, uzemanyag, tulajdonos, sofor);
+    }
 }
